3440cb225a4fc3846a6998393c245049d712f16e
Rod Landaeta <rodrigoelp@gmail.com>
2019-09-15 15:51:03 +1000

    Initial Commit

---
+++ swiftUIQuery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIQuery
+//
+//  Created by Rod Landaeta on 15/9/19.
+//  Copyright © 2019 Rod Landaeta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
